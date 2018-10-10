bb1476d9d8ee2594623265458e84d557cfdc97d8
annisaf <annisa@sepulsa.com>
2018-10-10 17:17:43 +0700

    Create logout screen

--- app/src/main/java/com/example/aferyannie/learningapp/LogoutActivity.java
+++ app/src/main/java/com/example/aferyannie/learningapp/LogoutActivity.java
@@ -1,13 +1,59 @@
 package com.example.aferyannie.learningapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class LogoutActivity extends AppCompatActivity {
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogoutActivity extends Fragment {
+    private FirebaseAuth mAuth;
+
+    Button logoutButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_logout, null, false);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutButton = (Button) view.findViewById(R.id.btnLogout);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAuth != null){
+                    LoginManager.getInstance().logOut();
+                    mAuth.signOut();
+                    Toast.makeText(getContext(), "Logged out successfully", Toast.LENGTH_SHORT).show();
+                    updateUI();
+                }
+            }
+        });
+        return view;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logout);
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null) {
+            updateUI();
+        }
+    }
+
+    private void updateUI() {
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new LoginActivity()).commit();
     }
 }
